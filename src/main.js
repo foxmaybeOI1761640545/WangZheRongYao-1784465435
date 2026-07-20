17eb8c374425db0b8c9ffa137739577ba78369ff
Yu Zhou <vathisasidharan+1761640545@gmail.com>
2026-07-21 01:10:36 +0800

    feat(android): 启动原生应用桥接与输入导航 | Start native bridge and input navigation

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import './style.css'
 import './layout.css'
 import './server-list.css'
 import './refinements.css'
+import './android.css'
 import App from './App.vue'
+import {
+  installFormEnterNavigation,
+  installNativeBackBridge,
+} from './platform/nativeAppShell.js'
+
+installFormEnterNavigation()
+void installNativeBackBridge()
 
 createApp(App).mount('#app')
