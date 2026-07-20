855a6ce9c7e4c73382300616e52892ad0616acc9
Yu Zhou <vathisasidharan+1761640545@gmail.com>
2026-07-20 19:29:04 +0800

    style(layout): 启用紧凑全宽样式覆盖 | Enable compact full-width refinements

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import './layout.css'
 import './server-list.css'
+import './refinements.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
