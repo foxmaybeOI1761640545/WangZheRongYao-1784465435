3f715d37b42ca7f72493df610c7bd993d6dc524d
Yu Zhou <vathisasidharan+1761640545@gmail.com>
2026-07-21 20:37:44 +0800

    ui(layout): 加载主页固定布局样式 | Load fixed home layout styles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './style.css'
 import './layout.css'
 import './server-list.css'
 import './refinements.css'
+import './home-layout.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
